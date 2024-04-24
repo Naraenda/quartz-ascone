5adca63ad85422fa8293f385a72f294af739ecb6
Naraenda <naraenda@prasetya.eu>
2024-04-24 22:12:01 +0200

    style: change style to look like old ascone

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -3,18 +3,37 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
 
+const ascii_art = `
+ _    _,  _, _, ,  ,  _, 
+'|\\  (_, /  / \\,|\\ | /_, 
+ |-\\  _)'\\_'\\_/ |'\\|'\\_  
+  '  \`'     \`'   '  \`   \` 
+ nara's online home
+      for random things
+`
+
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
   return (
     <h1 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
+      <a href={baseDir}>
+        <pre>{ascii_art}</pre>
+      </a>
     </h1>
   )
 }
 
 PageTitle.css = `
 .page-title {
+  font-size: 1em;
+  margin: 0;
+}
+.page-title a {
+  color: black;
+  text-align: center;
+}
+.page-title pre {
   margin: 0;
 }
 `
